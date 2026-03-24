b64700f28e320686b959b373f5f09e184062be17
lucas-c-lin <happylinchen@163.com>
2026-03-24 21:45:06 +0800

    change: search tips to chinese

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -48,9 +48,24 @@ export default defineConfig({
     search: {
       provider: 'local',
       options: {
-        placeholder: '搜索'
+        placeholder: '搜索',
+        translations: {
+          button: {
+            buttonText: '搜索',
+            buttonAriaLabel: '搜索文档'
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除搜索条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭'
+            }
+          }
+        }
       }
-    },
+    }
     nav: [
       { text: '首页', link: '/' },
       { text: '内容A', link: '/content_A/A1' },
